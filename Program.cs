acc060ea92ac4e120d33e9a47d44af511acf7a31
abrildn <abrudomingueznoya@gmail.com>
2025-09-06 20:05:24 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Doss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la nota del alumno");
+            string linea = Console.ReadLine();
+            float nota = float.Parse(linea);
+
+            float siete = 7;
+            if (nota >= siete)
+            {
+                Console.WriteLine("Promocionado");
+            }
+            else
+            {
+                Console.WriteLine("Desaprobado");
+            }
+            Console.ReadKey();
+        }
+    }
+}
